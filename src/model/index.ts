4de25aeac3d4b2c35fbebf25bd7baae5766cafd9
Shi Ling <shiling9386@gmail.com>
2023-06-07 11:04:39 +0800

    add type

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,7 +1,7 @@
 import { Bookmark, UsageExample } from "@prisma/client";
 
 export type SentenceExamplePayload = Pick<UsageExample, "bookmarkId" | "sentence">;
-export type BookmarkPayload = Pick<Bookmark, "word" | "description" | "createdBy"> & {
+export type BookmarkPayload = Pick<Bookmark, "word" | "description" | "createdBy" | "type"> & {
   createdAt?: Date;
   usageExamples?: string[];
 };
