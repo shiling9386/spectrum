8becd39463fa8a0fa876b62b8d8a294d80e5bd5a
Shi Ling <shiling9386@gmail.com>
2023-07-09 22:46:12 +0800

    change fn name

--- src/neetcode/two_pointers/2.twosum_sorted.ts
+++ src/neetcode/two_pointers/2.twosum_sorted.ts
@@ -1,4 +1,4 @@
-function twoSum(numbers: number[], target: number): number[] {
+function twoSumSorted(numbers: number[], target: number): number[] {
   let l = 0;
   let r = numbers.length - 1;
   while (l <= r) {
@@ -15,4 +15,4 @@ function twoSum(numbers: number[], target: number): number[] {
 }
 const numbers = [2, 7, 11, 15];
 const target = 9;
-twoSum(numbers, target);
+twoSumSorted(numbers, target);
